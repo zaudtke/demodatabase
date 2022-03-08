7b45ab0332c147e5e2a731309a0af0ef6d001d44
Al Zaudtke <zaudtke@gmail.com>
2022-03-08 08:17:25 -0500

    intermediate checkin - machine switch

--- src/Demo.Database/Program.cs
+++ src/Demo.Database/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Demo.Database;
+
+var arguments = Environment.GetCommandLineArgs();
+
+if(ArgumentList.IsHelpCommand(arguments))
+{
+	DisplayHelp();
+	return 0;
+}
+
+
+return 0;
+
+// Helper Functions
+void DisplayHelp()
+{
+	Console.WriteLine("\r\nAvailable Commands");
+	foreach(var kvp in ArgumentList.Arguments)
+	{
+		Console.WriteLine($"{kvp.Key}\t{kvp.Value}");
+	}
+	Console.WriteLine();
+}
---
+++ test/Demo.Database.UnitTest/ArgumentListTest.cs
@@ -0,0 +1,32 @@
+using Demo.Database;
+using Xunit;
+
+namespace Demo.Database.UnitTest;
+public class ArgumentListTest
+{
+	[Theory(DisplayName = "ArgumentList.IsHelCommand Returns True/False correctly based on arguments")]
+	[InlineData(false,new string[]{""})]
+	[InlineData(false,new string[]{"  "})]
+	[InlineData(false,new string[]{"", "  "})]
+	[InlineData(false,new string[]{"nothelp", "--nh", "--halp"})]
+	[InlineData(false,new string[]{"-h"})]
+	[InlineData(false,new string[]{"-help"})]
+	[InlineData(false,new string[]{"?h"})]
+	[InlineData(false,new string[]{"?help"})]
+	[InlineData(true,new string[]{"--h"})]
+	[InlineData(true,new string[]{"--H"})]
+	[InlineData(true,new string[]{"--help"})]
+	[InlineData(true,new string[]{"--Help"})]
+	[InlineData(true,new string[]{"--HELP"})]
+	[InlineData(true,new string[]{"--h", "--otherflag"})]
+	[InlineData(true,new string[]{"--otherflag", "--h"})]
+	[InlineData(true,new string[]{"--help", "--otherflag"})]
+	[InlineData(true,new string[]{"otherflag", "--help"})]
+	[InlineData(true,new string[]{"--HELP", "--otherflag"})]
+	[InlineData(true,new string[]{"otherflag", "--HELP"})]
+	public void ArgumentList_IsHelpCommand_Returns_CorrectValue(bool expected, string[] args)
+	{
+		var sut = ArgumentList.IsHelpCommand(args);
+		Assert.Equal(expected, sut);
+	}
+}
